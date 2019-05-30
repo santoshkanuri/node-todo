fb8217d668e11349c98ff445540e92e5af74c224
santoshKanuri <santoshcool534@gmail.com>
2019-05-30 18:43:38 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://dev.risecorp.com:27017/Test',
+    remoteUrl : 'mongodb://dev.risecorp.com:27017/test2',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
